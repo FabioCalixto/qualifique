4ac67f155a4b52d0cbbe438b547df7e6a8161b17
fabiocalixto <fabiocalixto@gmail.com>
2021-12-12 17:00:00 +0100

    feat: add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import './styles.scss'
+
+const Footer = () => (
+    <div className="footer">
+       <p>Qualifique &copy; Todos os direitos reservados</p>
+       <p>Desenvolvido por <a href="https://github.com/FabioCalixto" target="_blank" rel="noreferrer">Fábio Calixto</a></p>
+    </div>
+)
+
+export default Footer
